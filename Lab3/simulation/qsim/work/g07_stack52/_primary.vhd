52d1fd4420453e9c6e32d99cdb7c489fad68b787
GarethDPeters <32077468+GarethDPeters@users.noreply.github.com>
2017-11-10 13:38:18 -0500

    Reset

--- Lab3/simulation/qsim/work/g07_stack52/_primary.vhd
+++ Lab3/simulation/qsim/work/g07_stack52/_primary.vhd
@@ -2,6 +2,10 @@ library verilog;
 use verilog.vl_types.all;
 entity g07_stack52 is
     port(
+        altera_reserved_tms: in     vl_logic;
+        altera_reserved_tck: in     vl_logic;
+        altera_reserved_tdi: in     vl_logic;
+        altera_reserved_tdo: out    vl_logic;
         full            : out    vl_logic;
         num             : out    vl_logic_vector(5 downto 0);
         clk             : in     vl_logic;
@@ -10,7 +14,6 @@ entity g07_stack52 is
         address         : in     vl_logic_vector(5 downto 0);
         mode            : in     vl_logic_vector(1 downto 0);
         reset           : in     vl_logic;
-        sset_test       : out    vl_logic;
         value           : out    vl_logic_vector(5 downto 0);
         dataIn          : in     vl_logic_vector(5 downto 0)
     );
