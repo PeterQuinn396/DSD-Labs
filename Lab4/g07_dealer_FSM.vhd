dff9a834c5e9e55d7e9bcb43146981073ec087c8
PeterQuinn396 <peter.quinn396@gmail.com>
2017-11-15 17:42:14 -0500

    finished a first draft of dealer circuit
    Wrote code for dealer that compiles, still needs to be tested to ensure its working properly

--- Lab4/g07_dealer_FSM.vhd
+++ Lab4/g07_dealer_FSM.vhd
@@ -8,7 +8,7 @@ entity g07_dealer_FSM is
 port (request_deal: in std_logic;
 		reset:in std_logic;
 		clock:in std_logic;
-		stack_enable:out std_logic;)
+		stack_enable:out std_logic);
 
 end g07_dealer_FSM;
 
@@ -16,4 +16,4 @@ architecture g07_dealer_FSM_arch of g07_dealer_FSM is
 
 begin
 
-end architecture
+end architecture;
--- Lab4/g07_rules.vhd
+++ Lab4/g07_rules.vhd
@@ -1,5 +1,6 @@
 library ieee; 
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 library lpm;
 use lpm.lpm_components.all;
 
@@ -7,7 +8,8 @@ entity g07_rules is
 
 port (play_pile_top_card: in std_logic_vector(5 downto 0);
 		card_play: in std_logic_vector(5 downto 0);
-		legal_play: out std_logic);
+		legal_play: out std_logic;
+		sum: out std_logic_vector(5 downto 0));
 
 end g07_rules;
 
@@ -15,63 +17,76 @@ end g07_rules;
 architecture g07_rules_arch of g07_rules is
 
 component g07_Modulo_13
-	port (dataa : in std_logic_vector(6 downto 0);
+	port (A : in std_logic_vector(5 downto 0);
 		 floor13 : out std_logic_vector(2 downto 0);
 		 Amod13: out std_logic_vector(3 downto 0)
 	);
 	
 end component;
 
-component add_card_values
-	port (dataa : in std_logic_vector(4 downto 0);
-		 datab : in std_logic_vector(4 downto 0);
-		 total: out std_logic_vector(4 downto 0)
-	);
-
 
 signal new_card_value: std_logic_vector(3 downto 0);
 signal new_card_suit: std_logic_vector(2 downto 0);
-signal old_card_value: std_logic_vector(3 downto 0);
-signal old_card_suit: std_logic_vector(2 downto 0);
+signal old_sum: std_logic_vector(5 downto 0); --MSB is ace boolean, 5 other bits are previous total
+
 
 signal new_card_value_int: integer range 0 to 15;
-signal old_card_value_int: integer range 0 to 15;
-signal sum: integer range 0 to 63;
+signal old_sum_int: integer range 0 to 31;
+signal new_sum_int: integer range 0 to 63;
+
+signal ace: std_logic;
+
+begin
+
+old_sum<=play_pile_top_card(5 downto 0); --grab the old sum
+
+u1: g07_Modulo_13 port map (A=>card_play,floor13=>new_card_suit,Amod13=>new_card_value); -- parse the new card input
+
+process (old_sum,new_card_value)
+
+begin
+
+new_card_value_int <= to_integer(unsigned(new_card_value))+1; --add 1 to the card value to correct it  
+old_sum_int <= to_integer(unsigned(old_sum(4 downto 0))); -- convert to intgers to make life easier
+
+if (new_card_value_int > 10) then 
+	new_card_value_int<=10; --correct for face value cards
+elsif (new_card_value_int=1) then 
+	new_card_value_int<=11;	--correct for ace
+end if;
+
+ace<=old_sum(5); --check if we had an ace from a past hand
  
-
-begin
-
-getInput process
-
-begin
-u1: g07_Modulo_13 port map (dataa=>card_play, floor13=>new_card_suit, Amod13=>new_card_value); --parse the new card input
-u2: g07_Modulo_13 port map (dataa=>play_pile_top_card, floor13=>old_card_suit, Amod13=>old_card_value); --parse the old card input
-
-new_card_value_int <= to_integer(unsigned(new_card_value))+1; --convert to intgers to make life easier
-old_card_value_int <= to_integer(unsigned(old_card_value))+1; --add 1 to the card values to correct them
-
-if (new_card_value > 10) then new_card_value<=10;
-if (old_card_value > 10) then old_card_value<=10;
+new_sum_int <= old_sum_int+new_card_value_int; --compute the new sum
 
 
-end process
+ --check if the play is legal
+if (new_sum_int <= 21) then --see if our sum is legal as is
+	legal_play <= '1';
+	new_sum_int <= new_card_value_int+old_sum_int;
+	if (new_sum_int=11) then ace<='1'; --check if we got an new ace
+	end if;
 
-isLegalPlay process
-
-begin
-if (new_card_value_int+old_card_value_int>21)then 
-	legal_play <= '0';
-	sum <= new_card_value_int+old_card_value_int;
-
-elsif (new_card_value_int=1 or old_card_value_int=1) then --check for an ace
+elsif (new_card_value_int=11) then --check for a new ace, so we can fix our sum
+	new_sum_int<=new_sum_int-10;
+	legal_play<='1';
 		
-		if (new_card_value_int+old_card_value_int+10<21) then --took ace as 11 and it still fit
-			
-			sum <= new_card_value_int+old_card_value_int+10;
-		else legal_play<=1	
-else legal_play<='1'; 
+elsif (ace='1') then --check for an old ace
+	new_sum_int<=new_sum_int-10;
+	legal_play<='1';
+	ace<='0'; --remove the ace
+		
+else 
+	legal_play<='0'; 
+
+end if;
 
 
-end process
+sum <= ace&std_logic_vector(to_unsigned(new_sum_int,5));
+
+
+end process;
+
+
 
 end architecture;
