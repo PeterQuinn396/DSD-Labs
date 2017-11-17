17242c28245192314711d67c7316826e5d7320bd
PeterQuinn396 <peter.quinn396@gmail.com>
2017-11-16 21:25:25 -0500

    tested rules, it works

--- Lab4_fix/g07_rules_v2.vhd
+++ Lab4_fix/g07_rules_v2.vhd
@@ -4,17 +4,17 @@ use ieee.numeric_std.all;
 library lpm;
 use lpm.lpm_components.all;
 
-entity g07_rules is
+entity g07_rules_v2 is
 
 port (play_pile_top_card: in std_logic_vector(5 downto 0);
 		card_play: in std_logic_vector(5 downto 0);
 		legal_play: out std_logic;
 		sum: out std_logic_vector(5 downto 0));
 
-end g07_rules;
+end g07_rules_v2;
 
 
-architecture g07_rules_arch of g07_rules is
+architecture g07_rules_arch_v2 of g07_rules_v2 is
 
 component g07_Modulo_13
 	port (A : in std_logic_vector(5 downto 0);
@@ -30,11 +30,11 @@ signal new_card_suit: std_logic_vector(2 downto 0);
 signal old_sum: std_logic_vector(5 downto 0); --MSB is ace boolean, 5 other bits are previous total
 
 
-signal new_card_value_int: integer range 0 to 15:=0;
-signal new_card_value_int_raw: integer range 0 to 15:=0;
-signal old_sum_int: integer range 0 to 30:=0;
-signal new_sum_int: integer range 0 to 63:=0;
-signal test_new_sum_int: integer range 0 to 63:=0;
+signal new_card_value_int: unsigned (3 downto 0);
+signal new_card_value_int_raw: unsigned (3 downto 0);
+signal old_sum_int: unsigned (4 downto 0);
+signal new_sum_int: unsigned (4 downto 0);
+signal test_new_sum_int: unsigned (4 downto 0);
 
 
 signal old_ace: std_logic :='0';
@@ -51,44 +51,44 @@ begin
 old_sum<=play_pile_top_card(5 downto 0); --grab the old sum
 old_ace<=old_sum(5); --check if we had an ace from a past hand
 
-new_card_value_int_raw <= (to_integer(unsigned(new_card_value(3 downto 0)))+1); --add 1 to the card value to correct it  
-old_sum_int <= to_integer(unsigned(old_sum(4 downto 0))); -- convert to intgers to make life easier
+new_card_value_int_raw <= unsigned(new_card_value(3 downto 0))+"0001"; --add 1 to the card value to correct it  
+old_sum_int <= unsigned(old_sum(4 downto 0)); -- convert to intgers to make life easier
 
-if (new_card_value_int_raw > 10) then 
-	new_card_value_int<=10; --correct for face value cards
-elsif (new_card_value_int_raw=1) then 
-	new_card_value_int<=11;	--correct for ace
+if (new_card_value_int_raw > "1010") then 
+	new_card_value_int<="1010"; --correct for face value cards
+elsif (new_card_value_int_raw="0001") then 
+	new_card_value_int<="1101";	--correct for ace, this has to go in backwards for some reason
 else new_card_value_int<=new_card_value_int_raw;
 end if;
  
 test_new_sum_int <= new_card_value_int+old_sum_int; --compute the new sum
 
  --check if the play is legal
-if (test_new_sum_int < 22) then --see if our sum is legal as is
+if (test_new_sum_int < "10110") then --see if our sum is legal as is (less than 22)
 	legal_play <= '1';
 	new_sum_int<=test_new_sum_int;
-	if (new_card_value_int=11) then new_ace<='1'; --check if we got an new ace
+	if (new_card_value_int="1011") then new_ace<='1'; --check if we got an new ace
 		else new_ace<=old_ace;
 	end if;
 
-elsif (new_card_value_int=11) then --check for a new ace, so we can fix our sum
-	new_sum_int<=test_new_sum_int-10;
+elsif (new_card_value_int="1011") then --check for a new ace, so we can fix our sum
+	new_sum_int<=test_new_sum_int-"01010";
 	legal_play<='1';
 	new_ace<=old_ace;
 	
 elsif (old_ace='1') then --check for an old ace
-	new_sum_int<=test_new_sum_int-10;
+	new_sum_int<=test_new_sum_int-"01010";
 	legal_play<='1';
 	new_ace<='0'; --remove the ace
 		
 else 
 	legal_play<='0'; --no aces and over 21, so illegal play
 	new_ace<=old_ace;
-	new_sum_int<=31;
+	new_sum_int<="11111";
 end if;
 
 end process;
 
-sum <= new_ace&std_logic_vector(to_unsigned(new_sum_int,5));
+sum <= new_ace&std_logic_vector(new_sum_int);
 
 end architecture;
