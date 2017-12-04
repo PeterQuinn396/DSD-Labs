091f40b6aff71346f401e32033c41ebf90137c7c
PeterQuinn396 <peter.quinn396@gmail.com>
2017-12-03 22:27:00 -0500

    got everythig working doing massive rewrites
    I think it all works now. Had to seperate the drawing a card and the adding a card to the hand system to get everything to line up properly

--- Lab5/g07_computer_FSM.vhd
+++ Lab5/g07_computer_FSM.vhd
@@ -8,18 +8,19 @@ use lpm.lpm_components.all;
 entity g07_computer_FSM is
 port(dealer_turn,clk,reset_all: in std_logic;
 		dealer_sum:in std_logic_vector (5 downto 0);
+		new_card:in std_logic_vector (5 downto 0);
 		dealer_done:out std_logic;
-		request_card:out std_logic);
+		request_card,add_card_to_hand:out std_logic);
 end g07_computer_FSM;
 
 architecture g07_computer_FSM_arc of g07_computer_FSM is
 
-	TYPE state_signal IS (WAIT_TURN,CP_TURN,DRAW,END_TURN);
+	TYPE state_signal IS (WAIT_TURN,CP_TURN,DRAW,END_TURN,LOAD_CARD);
 	signal state: state_signal;
 	signal vec_changed: std_logic;
 	signal old_sum_int: integer range 0 to 31:=0;
 	signal old_sum_last: std_logic_vector(5 downto 0);
-	
+	signal last_card: std_logic_vector(5 downto 0);
 begin
 	update: process(clk,reset_all)
 	begin
@@ -33,10 +34,16 @@ begin
 			when WAIT_TURN =>
 				if dealer_turn = '1' then state<=CP_TURN; end if;
 			when CP_TURN=>
-				
 				if (old_sum_int > 16) then state<=END_TURN;
-				else state <= DRAW;
+				else state <= LOAD_CARD;
 				end if;
+		
+			when LOAD_CARD =>
+				vec_changed <= or_reduce(new_card xor last_card);  --see if the sum has changed (i.e. wait until it has changed)
+				if vec_changed = '1' then -- wait until the sum changes and we don't have the default value
+					state<=DRAW; --put card in players hand
+				end if;
+			
 			when DRAW =>
 				vec_changed <= or_reduce(old_sum_last xor dealer_sum);
 				if vec_changed = '1' then --wait until the sum changes
@@ -47,24 +54,35 @@ begin
 			end case;
 			
 			old_sum_last <= dealer_sum;
-			
+			last_card <=new_card;
 		end if;
 	end process;
 	output_logic: process(state,clk)
 	begin
+	
 			case state is
+				
 			when WAIT_TURN =>
 				dealer_done <= '0';
 				request_card<='0';
+				add_card_to_hand<='0';
 			when CP_TURN=>
 				dealer_done <= '0';
 				request_card<='0';
+				add_card_to_hand <='0';
 			when DRAW=>
 				dealer_done <= '0';
-				request_card <='1';
+				request_card <='0';
+				add_card_to_hand <='1';
 			when END_TURN=>
 				dealer_done <= '1';
 				request_card <='0';
+				add_card_to_hand <='0';
+			when LOAD_CARD =>
+				dealer_done <= '0';
+				request_card<='1';
+				add_card_to_hand <='0';
+				
 			end case;
 	end process;
 end architecture;
--- Lab5/g07_controller_FSM.vhd
+++ Lab5/g07_controller_FSM.vhd
@@ -9,19 +9,20 @@ entity g07_controller_FSM is
 
 port (hit, stay, d_bust, p_bust, d_done, reset_sig, clock: in std_logic;
 		d_sum, p_sum, new_card: in std_logic_vector(5 downto 0);
-		en_p, en_d, p_won, d_won, init, d_turn,p_turn, rand_init,load,en_p_first: out std_logic
+		en_p, en_d, p_won, d_won, init, d_turn,p_turn, rand_init,load: out std_logic
 		);
 
 end g07_controller_FSM;
 
 architecture g07_controller_FSM_arch of g07_controller_FSM is
 	TYPE state_signal IS (INIT_RAND,WAIT_RAND,RESET_DECK, DEALER_CARD_1, DEALER_CARD_2, PLAYER_CARD_1, 
-	PLAYER_CARD_2, PLAYER_HIT, PLAYER_CHOOSE, DEALER_TURN, PLAYER_WON, DEALER_WON, ENDGAME, RESET_WAIT,LOAD_FIRST_CARD);
+	PLAYER_CARD_2, PLAYER_HIT, PLAYER_CHOOSE, DEALER_TURN, PLAYER_WON, DEALER_WON, ENDGAME, RESET_WAIT,
+	LOAD_PLAYER_CARD, LOAD_PLAYER_CARD_2, LOAD_PLAYER_CARD_1,LOAD_DEALER_CARD_1,LOAD_DEALER_CARD_2);
 	signal state: state_signal;
 	signal vec_changed: std_logic;
 	signal p_sum_last: std_logic_vector (5 downto 0);
 	signal d_sum_last: std_logic_vector (5 downto 0);
-	
+	signal last_card: std_logic_vector (5 downto 0);
 	signal counter: integer range 0 to 15;
 	
 begin
@@ -50,35 +51,50 @@ begin
 			when RESET_WAIT =>
 				if counter = 12 then --wait 5 clock cycles for stack to completly initialize (delay due to POP_EN and other things)
 					counter <= 0;
-					state <= LOAD_FIRST_CARD;
+					state <= LOAD_PLAYER_CARD_1;
 				else counter <= counter+1;
 				end if;
 				
-			when LOAD_FIRST_CARD =>
-				if new_card /= "000000" or counter = 12 then --wait for first card to load in
-					counter<=0;
+			when LOAD_PLAYER_CARD_1 =>
+				vec_changed <= or_reduce(new_card xor last_card);  --see if the sum has changed (i.e. wait until it has changed)
+				if vec_changed = '1' then -- wait until the sum changes and we don't have the default value
 					state<=PLAYER_CARD_1; --put card in players hand
-				else counter<=counter+1;
 				end if;
 				
-			
 			when PLAYER_CARD_1 =>
 				vec_changed <= or_reduce(p_sum xor p_sum_last);  --see if the sum has changed (i.e. wait until it has changed)
 				if vec_changed = '1' then -- wait until the sum changes and we don't have the default value
-					state <= DEALER_CARD_1;
+					state <= LOAD_DEALER_CARD_1;
+				end if;
+				
+			when LOAD_DEALER_CARD_1 =>
+				vec_changed <= or_reduce(new_card xor last_card);  --see if the sum has changed (i.e. wait until it has changed)
+				if vec_changed = '1' then -- wait until the sum changes and we don't have the default value
+					state<=DEALER_CARD_1; --put card in players hand
 				end if;
 				
 			when DEALER_CARD_1 =>
-				vec_changed <= or_reduce(d_sum xor d_sum_last);  --see if the sum has changed 
-																				--(i.e. wait until it has changed)
+				vec_changed <= or_reduce(d_sum xor d_sum_last);
 				if vec_changed = '1' then 
-					state <= PLAYER_CARD_2; 
+					state <= LOAD_PLAYER_CARD_2; 
+				end if;
+				
+			when LOAD_PLAYER_CARD_2 =>
+				vec_changed <= or_reduce(new_card xor last_card);  --see if the sum has changed (i.e. wait until it has changed)
+				if vec_changed = '1' then -- wait until the sum changes and we don't have the default value
+					state<=PLAYER_CARD_2; --put card in players hand
 				end if;
 				
 			when PLAYER_CARD_2 =>
 				vec_changed <= or_reduce(p_sum xor p_sum_last);  --see if the sum has changed (i.e. wait until it has changed)
 				if vec_changed = '1' then 
-					state <= DEALER_CARD_2; 
+					state <= LOAD_DEALER_CARD_2; 
+				end if;
+				
+			when LOAD_DEALER_CARD_2 =>
+				vec_changed <= or_reduce(new_card xor last_card);  --see if the sum has changed (i.e. wait until it has changed)
+				if vec_changed = '1' then -- wait until the sum changes and we don't have the default value
+					state<=DEALER_CARD_2; --put card in players hand
 				end if;
 				
 			when DEALER_CARD_2 =>
@@ -90,19 +106,24 @@ begin
 			when PLAYER_CHOOSE =>
 				if to_integer(unsigned(d_sum(4 downto 0)))=21 then  --check if dealer got dealt 21, if so, he wins
 						state<= DEALER_WON;
+				elsif p_bust = '1' then
+						state <= DEALER_WON; --dealer automatically wins if player busts
 				elsif hit='1' then 
-					state <= PLAYER_HIT;
+					state <= LOAD_PLAYER_CARD;
 				elsif stay='1' then 
 					state <= DEALER_TURN;
 				end if;		
-	
+			
+			when LOAD_PLAYER_CARD =>
+				vec_changed <= or_reduce(new_card xor last_card);  --see if the sum has changed (i.e. wait until it has changed)
+				if vec_changed = '1' then -- wait until the sum changes and we don't have the default value
+					state<=PLAYER_HIT; --put card in players hand
+				end if;
+			
 			WHEN PLAYER_HIT =>
 				vec_changed <= or_reduce(p_sum xor p_sum_last);  --see if the sum has changed (i.e. wait until it has changed)
 				if vec_changed = '1' then --wait until the sum changes
-					if p_bust = '1' then
-						state <= DEALER_WON; --dealer automatically wins if player busts
-						else state <= PLAYER_CHOOSE; 
-					end if;
+					state <= PLAYER_CHOOSE; 
 				end if;
 				
 			when DEALER_TURN =>
@@ -132,6 +153,7 @@ begin
 			--state that needs them
 			p_sum_last<=p_sum;
 			d_sum_last<=d_sum;
+			last_card<=new_card;
 			
 		end if;
 	end process;
@@ -148,8 +170,7 @@ begin
 		p_turn <= '0';
 		rand_init<='0';
 		load<='0';
-		en_p_first<='0';
-		
+	
 		case state is
 			
 			when INIT_RAND =>
@@ -165,7 +186,7 @@ begin
 				en_d <= '1';
 								
 			when PLAYER_CARD_1 =>		
-				en_p_first <= '1';
+				en_p <= '1';
 			
 			when PLAYER_CARD_2 => 
 				en_p <= '1';
@@ -191,8 +212,8 @@ begin
 			 --do nothing
 			when RESET_WAIT =>
 			--nothing
-			when LOAD_FIRST_CARD=>
-				load<='1';
+			when others => --all the loads
+			load <= '1';
 				
 		end case;
 	end process;	
